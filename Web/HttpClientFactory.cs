e2ef671ecaef000ae011d4a4e3a15a525ccdd370
felixkmh <24227002+felixkmh@users.noreply.github.com>
2022-08-20 19:49:56 +0200

    fix: check for deleted icons

---
+++ Web/HttpClientFactory.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PlayniteCommon.Web
+{
+    public class HttpClientFactory
+    {
+        private static Lazy<IHttpClientFactory> httpClientFactory = new Lazy<IHttpClientFactory>(InitHttpClientFactory);
+
+
+        private static IHttpClientFactory InitHttpClientFactory()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddHttpClient("HandleNoUseCookies")
+                             .ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler { UseCookies = false });
+
+            var serviceProvide = serviceCollection.BuildServiceProvider();
+
+            return serviceProvide.GetService<IHttpClientFactory>();
+        }
+
+        public static HttpClient GetClient()
+        {
+            return httpClientFactory.Value.CreateClient();
+        }
+    }
+}
