5e041eff9245a1b883e2b618da55147035a75085
felixkmh <24227002+felixkmh@users.noreply.github.com>
2022-08-20 19:56:42 +0200

    addded extensions and constants

---
+++ Extensions/StrintExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayniteCommon.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string s)
+        {
+            return string.IsNullOrEmpty(s);
+        }
+    }
+}
