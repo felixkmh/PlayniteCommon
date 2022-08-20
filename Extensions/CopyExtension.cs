2941570ba49fb6cfcefd0ee81bbd1f308a21e273
felixkmh <24227002+felixkmh@users.noreply.github.com>
2022-08-20 21:13:42 +0200

    added extensions from StartPage

---
+++ Extensions/CopyExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayniteCommon.Extensions
+{
+    public static class CopyExtension
+    {
+        public static T Copy<T>(this T original) where T : class
+        {
+            return Playnite.SDK.Data.Serialization.FromJson<T>(Playnite.SDK.Data.Serialization.ToJson(original));
+        }
+    }
+}
